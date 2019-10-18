3c93f03fa51a23bba59038627c5398781d2b2529
Ashish tiwari <ashish@salezshark.com>
2019-10-18 12:01:44 +0530

    color background fix

--- apps/feedback/src/app/features/main-app/feedback/component/feedback.component.ts
+++ apps/feedback/src/app/features/main-app/feedback/component/feedback.component.ts
@@ -18,5 +18,27 @@ export class FeedbackComponent implements OnInit {
     this.store.dispatch(new DeleteFeedback(feedback._id))
     console.log(feedback)
   }
-
+  invertColor(hex, bw) {
+    if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+    }
+    const r = parseInt(hex.slice(0, 2), 16) as any;
+    const g = parseInt(hex.slice(2, 4), 16) as any;
+    const b = parseInt(hex.slice(4, 6), 16) as any;
+    return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+      ? '#000000'
+      : '#FFFFFF';
+  }
+  padZero(str, len?) {
+    len = len || 2;
+    const zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+  }
 }
